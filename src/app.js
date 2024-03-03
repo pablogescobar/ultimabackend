8900500537b9e11ade893a44a659ce5823acaff4
Federico Di Iorio <federico_metallica@hotmail.com>
2024-03-03 14:17:02 -0300

    Primeros pasos

---
+++ src/app.js
@@ -0,0 +1,41 @@
+const productsRouter = require('./routes/products.router');
+const express = require('express');
+const app = express();
+
+app.use('/api/products', productsRouter);
+
+// app.get('/products', async (req, res) => {
+//     try {
+//         const products = await manager.getProducts();
+//         const limitFilter = +req.query.limit;
+
+//         if (limitFilter <= 0 || isNaN(parseInt(limitFilter))) {
+//             res.status(400).json({ error: 'Debe ingresar un número válido superior a 0.' });
+//             return;
+//         }
+
+//         if (limitFilter) {
+//             const limit = parseInt(limitFilter);
+//             const limitedProducts = products.slice(0, limit);
+//             res.json(limitedProducts);
+//         } else {
+//             res.json(products);
+//         }
+//     } catch {
+//         res.json({ Error: 'Error al cargar los productos' });
+//     };
+// });
+
+// app.get('/products/:pid', async (req, res) => {
+//     try {
+//         const productId = parseInt(req.params.pid);
+//         const product = await manager.getProductById(productId);
+//         product ? res.json(product) : res.json('El producto no existe');
+//     } catch {
+//         res.json({ Error: 'Error al buscar el id' });
+//     }
+// });
+
+app.listen(8080, () => {
+    console.log('Server Listo!');
+});
