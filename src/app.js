d0594b48e2d591d2eaf38634bd4b4d78e7097863
Federico Di Iorio <federico_metallica@hotmail.com>
2024-03-27 18:44:00 -0300

    El sistema de archivos ahora funciona de manera correcta

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const handlebars = require('express-handlebars');
 
 const ProductManager = require('./dao/fileManagers/ProductManager');
+const CartManager = require('./dao/fileManagers/CartManager');
 
 const productsRouter = require('./routes/products.router');
 const cartRouter = require('./routes/cart.router');
@@ -20,15 +21,19 @@ app.use(express.static(`${__dirname}/../public`))
 
 // Se asignan las rutas para los endpoints relacionados con los productos y el carrito
 app.use('/api/products', productsRouter); // Rutas relacionadas con los productos
-// app.use('/api/cart', cartRouter); // Rutas relacionadas con el carrito
+app.use('/api/cart', cartRouter); // Rutas relacionadas con el carrito
 
 // Se inicia el servidor en el puerto 8080
 
 const main = async () => {
 
-    const productManager = new ProductManager(`${__dirname}/../assets/products.json`)
+    const productManager = new ProductManager(`${__dirname}/../assets/products.json`);
     await productManager.getProducts();
-    app.set('productManager', productManager)
+    app.set('productManager', productManager);
+
+    const cartManager = new CartManager(`${__dirname}/../assets/cart.json`);
+    await cartManager.getCarts();
+    app.set('cartManager', cartManager);
 }
 app.listen(8080, () => { console.log('Servidor cargado!') });
 
