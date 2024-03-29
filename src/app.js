68d01f0785ce04400046c813fc7e0c0aec07fe28
Federico Di Iorio <federico_metallica@hotmail.com>
2024-03-29 17:02:15 -0300

    Se removió el addProducts.router

--- src/app.js
+++ src/app.js
@@ -6,7 +6,6 @@ const CartManager = require('./dao/fileManagers/CartManager');
 
 const productsRouter = require('./routes/products.router');
 const cartRouter = require('./routes/cart.router');
-const addProductRouter = require('./routes/addProduct.router');
 
 const app = express();
 
@@ -22,7 +21,6 @@ app.use(express.static(`${__dirname}/../public`))
 
 // Se asignan las rutas para los endpoints relacionados con los productos y el carrito
 app.use('/api/products', productsRouter); // Rutas relacionadas con los productos
-app.use('/api/admin', addProductRouter); // Rutas relacionadas con los productos
 app.use('/api/cart', cartRouter); // Rutas relacionadas con el carrito
 
 // Se inicia el servidor en el puerto 8080
