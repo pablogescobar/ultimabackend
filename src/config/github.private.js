8ac148d0ce4c501c969e36f9b0432453ed40822f
Federico Di Iorio <federico_metallica@hotmail.com>
2024-05-13 15:44:14 -0300

    Cambio en variables de entorno de github

--- src/config/github.private.js
+++ src/config/github.private.js
@@ -1,7 +1,7 @@
 require('dotenv').config(); // Carga las variables de entorno desde .env
 module.exports = {
-    appId: '885990',
-    clientID: 'Iv1.35b1e7ec932fdfc7',
-    clientSecret: '8d96cdd75bd0cf38b86e6e235d06d86e1f469963',
-    callbackURL: 'http://localhost:8080/api/sessions/githubcallback'
+    appId: process.env.GH_APPID,
+    clientID: process.env.GH_CLIENTID,
+    clientSecret: process.env.GH_CLIENTSECRET,
+    callbackURL: process.env.GH_CALLBACK
 }
