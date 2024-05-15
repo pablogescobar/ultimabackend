4b209715c66e52c7ea83c41c11f3c8a566dddf6b
Federico Di Iorio <federico_metallica@hotmail.com>
2024-05-15 19:37:00 -0300

    Arquitectura por capas: sessions.contoller.js

--- src/routes/session.router.js
+++ src/routes/session.router.js
@@ -2,90 +2,27 @@ require('dotenv').config(); // Carga las variables de entorno desde .env
 const { Router } = require('express'); // Importa la clase Router de Express para definir las rutas
 const router = Router(); // Crea un enrutador
 const passport = require('passport');
-const cookieParser = require('cookie-parser');
-const { generateToken } = require('../utils/jwt');
-
-router.use(cookieParser());
-
-router.post('/register', passport.authenticate('register', { failureRedirect: '/api/sessions/failregister', session: false }), (_, res) => {
-    res.redirect('/');
-});
-
-router.post('/login', passport.authenticate('login', { failureRedirect: '/api/sessions/faillogin', session: false }), async (req, res) => {
-    try {
-        let user;
-        if (req.user && req.user.email === process.env.ADMIN_USER) {
-            user = req.user;
-        } else {
-            user = {
-                email: req.user.email,
-                _id: req.user._id.toString(),
-                rol: req.user.rol,
-                firstName: req.user.firstName,
-                lastName: req.user.lastName,
-                age: req.user.age,
-                cart: req.user.cart ? req.user.cart._id : null
-            };
-        }
-        const accessToken = generateToken(user);
-        res.cookie('accessToken', accessToken, { maxAge: 60 * 5 * 1000, httpOnly: true });
-        res.redirect('/');
-    } catch (e) {
-        res.status(500).json({ error: e.message });
-    }
-});
+const { Controller } = require('../controller/sessions.controller');
 
 
-router.get('/current', passport.authenticate('current', { session: false }), async (req, res) => {
-    return res.json(req.user)
-})
+router.post('/register', passport.authenticate('register', { failureRedirect: '/api/sessions/failregister', session: false }), (_, res) => new Controller().redirect(res));
+
+router.post('/login', passport.authenticate('login', { failureRedirect: '/api/sessions/faillogin', session: false }), async (req, res) => new Controller().login(req, res));
+
+router.get('/current', passport.authenticate('current', { session: false }), (req, res) => new Controller().current(req, res));
 
 router.get('/github', passport.authenticate('github', { scope: ['user:email'] }));
 
-router.get('/githubcallback', passport.authenticate('github', { session: false, failureRedirect: '/login' }), async (req, res) => {
-    try {
-        // Envía el token JWT al cliente
-        res.cookie('accessToken', req.user.accessToken, { maxAge: 24 * 60 * 60 * 1000, httpOnly: true });
-        res.redirect('/');
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
-})
+router.get('/githubcallback', passport.authenticate('github', { session: false, failureRedirect: '/login' }), (req, res) => new Controller().githubCb(req, res));
 
-router.get('/faillogin', (_, res) => {
-    res.send('Hubo un error de logeo.');
-})
+router.post('/resetPassword', passport.authenticate('resetPassword', { failureRedirect: '/api/sessions/failogin' }), async (_, res) => new Controller().redirect(res));
 
-router.post('/resetPassword', passport.authenticate('resetPassword', { failureRedirect: '/api/sessions/failogin' }), async (_, res) => {
-    try {
-        res.redirect('/');
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
-});
+router.get('/logout', (_, res) => new Controller().logout(res));
 
-router.get('/logout', (_, res) => {
-    res.clearCookie('accessToken'); // Elimina la cookie llamada 'accessToken'
-    res.redirect('/');
-});
+router.get('/failregister', (_, res) => new Controller().logError(res));
 
-router.get('/failregister', (_, res) => {
-    res.send('Hubo un error de registro.');
-})
+router.get('/faillogin', (_, res) => new Controller().logError(res));
 
-router.get('/faillogin', (_, res) => {
-    res.send('Hubo un error de logeo.');
-})
-
-router.delete('/', async (req, res) => {
-    try {
-        const { email } = req.body;
-        const userManager = req.app.get('userManager');
-        await userManager.deleteUser(email);
-        res.json({ message: 'Usuario eliminado correctamente.' });
-    } catch (e) {
-        res.status(500).json({ error: e.message });
-    }
-})
+router.delete('/', (req, res) => new Controller().deleteUser(req, res));
 
 module.exports = router;
